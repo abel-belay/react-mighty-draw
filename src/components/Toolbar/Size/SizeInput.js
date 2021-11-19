146bd4e443160c5f1e3ab2729b1d43379abf7996
Abel Belay <abel.belay01@gmail.com>
2021-11-19 02:15:24 -0700

    Minor module import fix.

--- src/components/Toolbar/Size/SizeInput.js
+++ src/components/Toolbar/Size/SizeInput.js
@@ -1,4 +1,4 @@
-import { useContext } from "react/cjs/react.development";
+import { useContext } from "react";
 import DrawContext from "../../../store/draw-context";
 import styles from "./SizeInput.module.css";
 
